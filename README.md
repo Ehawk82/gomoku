002eecc790d4d97488405e9091049c3c8fe5f6ec
Jason Graziano <mrehawk82@gmail.com>
2020-08-03 12:19:19 -0400

    ...

--- README.md
+++ README.md
@@ -2,10 +2,10 @@
 "a gomoku game in the making..."
 
 <p>Things to consider:</p>
-- 5 tile win(up, down, diag)
-- win detection
-- user indicators
-- clean, documentented, DRY code
-- 15x15 tile board
-- 19x19 tile board
-- 
+
+- [ ] 5 tile win(up, down, diag)
+- [ ] win detection
+- [ ] user indicators
+- [ ] clean, documentented, DRY code
+- [ ] 15x15 tile board(choice)
+- [ ] 19x19 tile board(choice)
--- docs/src/js/scripts.js
+++ docs/src/js/scripts.js
@@ -1,14 +1,57 @@
 var myUI;
 myUI = {
     init: function(){
-        var startPage = createEle("div");
+        var startPage = createEle("div"),
+        	gameChoice = createEle("div"),
+        	small = createEle("button"), large = createEle("button");
+
+        small.innerHTML = "15x15";
+        small.onclick = myUI.evalBoardChoice(15,startPage);
+
+        large.innerHTML = "19x19";
+        large.onclick = myUI.evalBoardChoice(19,startPage);
+
+        gameChoice.className = "gameChoice";
+        gameChoice.append(small,large);
 
         startPage.className = "startPage";
-        startPage.append("game will go here");
+        startPage.append(gameChoice);
 
         body.append(startPage);
+
+        setTimeout(function(){
+        	makeFull(startPage);
+        },0);
+    },
+    evalBoardChoice: function(c,startPage){
+    	return function(){
+    		takeFull(startPage);
+    		deleteThis(startPage,1001);
+    		setTimeout(function(){
+    			myUI.createBoard(c);
+        	},1002);
+    	}
+    },
+    createBoard: function(c){
+    	var tb = createEle("table");
+
+    	for (var i = 0; i < c; i++) {
+    		var tr = createEle("tr");
+
+    		for (var k = 0; k < c; k++) {
+    			var td = createEle("td");
+    			
+    			td.innerHTML = "⚫";
+
+    			tr.append(td);
+    		}
+
+    		tb.append(tr);
+    	}
+
+    	body.append(tb);
     }
-}
+};
 
 window.onload = function() {
     myUI.init();
